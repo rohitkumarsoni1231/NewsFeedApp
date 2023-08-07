5f02b06e217eef4c57e4a0bba57b916ab07443d3
Rohit Kumar <rohit.kumar@xynotech.com>
2023-08-07 19:41:06 +0500

    working on NewsFeed UI

---
+++ NewsFeedApp/Model/FeedData.swift
@@ -0,0 +1,14 @@
+//
+//  FeedData.swift
+//  NewsFeedApp
+//
+//  Created by Rohit Kumar on 8/7/23.
+//
+
+import Foundation
+
+struct FeedData {
+    var title: String
+    var subTitle: String
+    var timeStamp: Date
+}
--- NewsFeedApp/ViewController.swift
+++ NewsFeedApp/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var hours: [String] = []
+    var FeedData : [FeedData] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        hours = generateHourList()
+        tableView.dataSource = self
     }
-
-
+    
+    func generateHourList() -> [String] {
+        var hours: [String] = []
+        for hour in 1...12 {
+            let amHour = "\(hour) am"
+            hours.append(amHour)
+            if hour != 12 {
+                hours.append("   ")
+            }
+        }
+        for hour in 1...12 {
+            let pmHour = "\(hour) pm"
+            hours.append(pmHour)
+            if hour != 12 {
+                hours.append("   ")
+            }
+        }
+        return hours
+    }
+    
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+    }
+    
+    
+    
 }
 
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return hours.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HourCell", for: indexPath)
+        let hour = hours[indexPath.row]
+        cell.textLabel?.text = hour
+        return cell
+        
+    }
+}
